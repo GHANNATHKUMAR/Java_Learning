bb6e358705920be6627738ab92ade432da1939f7
Ghannath Kumar <ghannathk@email.com>
2026-03-02 21:17:24 +0530

    Day1 and Day2 added

---
+++ DAY1.java
@@ -0,0 +1,9 @@
+// basic
+// it is a programming language used in development and many more..
+class DAY1{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.print("Helllo world");
+        System.out.println("hello bhai");
+    }
+}
---
+++ DAY2.java
@@ -0,0 +1,20 @@
+public class DAY2 {
+    // this is second day of java
+    /* this is multiline comment 
+    in java  */
+     public static void main(String[] args){
+        String name="Ghannath"; // string is unmutable 
+        int a=20;
+        final int population=300000;
+        float myfloatnum=3.44f;
+        char myletter='A';
+        boolean istrue=true;
+        int x=5;
+        int y=7;
+        System.out.println(x+y);
+        System.out.println(name+x+y);
+
+        // must remember that identifiers i.e variables name should be according to rules in java 
+       int w=10;
+     }
+}
